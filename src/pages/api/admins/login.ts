d4e3633bf6915328dfc4eebf60295cf302052f4f
muhamadsony <sonyfadhil11@gmail.com>
2023-03-31 01:34:17 +0700

    🎉 Create Authentication Admin with iron-session

--- src/pages/api/admins/login.ts
+++ src/pages/api/admins/login.ts
@@ -2,28 +2,32 @@ import { withSessionRoute } from "../../../../lib/config/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
 import Admin from "../../../../models/admin";
 
+type AdminLogin = Pick<Admin, 'email' | 'password'>
+
 const loginRoute = async (req: NextApiRequest, res: NextApiResponse) => {
-
+    console.log(req.session.userId)
     if (req.method === "POST") {
-
-        const { email, password } = req.body
-        const admin = await Admin.findOne({ where: { username: email } })
+        const { email, password }: AdminLogin = req.body
+        const admin = await Admin.findOne({ where: { email: email } })
 
         if (admin === null) {
             res.status(404).send({ error: { email: "Invalid email" } })
         } else {
             const valid_password = await admin.valid_password(password)
-            if (!valid_password) {
+
+            if (valid_password) {
+                req.session.userId = admin.id
+                await req.session.save()
+                res.send({ ok: true })
+            } else {
                 res.status(404).send({ error: { password: "Invalid password" } })
             }
 
-            req.session.userId = admin.id
-            await req.session.save()
-            res.send({ ok: true })
 
         }
+    } else {
+        res.status(404).send("Method not provided")
     }
-    res.status(404).send("Method not provided")
 
 }
 
