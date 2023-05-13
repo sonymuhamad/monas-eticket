66fe215990bf2a65a1010ad21c7c03c12e97c805
sonymuhamad <sonyfadhil11@gmail.com>
2023-05-13 11:21:34 +0700

    fixing bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import type { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import id from "dayjs/locale/id";
-import { ruRU } from "@mui/x-date-pickers/locales";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -19,13 +17,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
-    <LocalizationProvider
-      adapterLocale={id}
-      localeText={
-        ruRU.components.MuiLocalizationProvider.defaultProps.localeText
-      }
-      dateAdapter={AdapterDayjs}
-    >
+    <LocalizationProvider adapterLocale={"id-ID"} dateAdapter={AdapterDayjs}>
       <Component {...pageProps} />
     </LocalizationProvider>
   );
